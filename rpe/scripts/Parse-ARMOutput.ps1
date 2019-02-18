580d2282f1cc5187f89bf0f5ff0cc63e248c693f
Leonardo Fernandez <leonfs@gmail.com>
2019-02-18 14:48:43 +0100

    Removed now unnecessary job as it is the same vm. Cleaned up file structures

--- rpe/scripts/Parse-ARMOutput.ps1
+++
@@ -1,8 +0,0 @@
-param (
-  [Parameter(Mandatory=$true)][string]$ARMOutput
-)
-
-$json = $ARMOutput | convertfrom-json
-$json.PSObject.Properties | ForEach-Object {
-    Write-Host "##vso[task.setvariable variable=$($_.name);]$($_.value.value)"
-}
