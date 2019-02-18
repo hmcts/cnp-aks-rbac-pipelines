be35c091c0a347780982ff31083c3f251fdfe06c
Leonardo Fernandez <leonfs@gmail.com>
2019-02-18 16:39:22 +0100

    Adding some quotes to fix JSON formatting

--- rpe/scripts/parse-arm-output.sh
+++ rpe/scripts/parse-arm-output.sh
@@ -7,7 +7,7 @@ if [ -z "${ARM_OUTPUT}" ] ; then
   exit 1
 fi
 
-for row in $(echo "${ARM_OUTPUT}" | jq -c '. | to_entries[]'); do
+for row in $(echo $ARM_OUTPUT | jq -c '. | to_entries[]'); do
   outputName=$(echo ${row} | jq -r ".key")
   outputValue=$(echo ${row} | jq -r ".value.value")
   echo "##vso[task.setvariable variable=${outputName}]${outputValue}"
