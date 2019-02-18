081622f34da49cddda32e395324c1c29e7caf645
Leonardo Fernandez <leonfs@gmail.com>
2019-02-18 13:27:01 +0100

    Using bash instead of Powershell

---
+++ rpe/scripts/check-vnet-prerequisites.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+RESOURCE_GROUP_NAME=$1
+VNET_CIDR=$2
+SUBNET_CIDR=$3
+
+if [ -z "${RESOURCE_GROUP_NAME}" ] ; then
+  echo "Resource Group name is required." 
+  exit 1
+fi
+
+if [ -z "${VNET_CIDR}" ] ; then
+  echo "VNET CIDR is required." 
+  exit 1
+fi
+
+if [ -z "${SUBNET_CIDR}" ] ; then
+  echo "SUBNET CIDR is required." 
+  exit 1
+fi
+
+vnet=$(az network vnet list -g ${RESOURCE_GROUP_NAME} --query "[?contains(addressSpace.addressPrefixes, '${VNET_CIDR}')][] | [0]")
+if [ -z "$vnet" ] ; then
+  echo "Continuing with a brand new infra..."
+  exit 0
+fi
+
+subnetCandidate=$(echo ${vnet} | jq -e '.subnets | map(select(.addressPrefix == "'${SUBNET_CIDR}'")) | first | select (.!=null)')
+if [ -z "$subnetCandidate" ] ; then
+  echo "Subnet ${SubnetCIDR} not present in vnet ${VNET_CIDR}"
+  exit 1
+fi
+
+subnetId=$(echo $subnetCandidate | jq ".id")
+echo "##vso[task.setvariable variable=aksSubnetId;isOutput=true]${subnetId}"
+
+
+
+
+
+
---
+++ rpe/scripts/parse-arm-output.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+ARM_OUTPUT=$1
+
+if [ -z "${ARM_OUTPUT}" ] ; then
+  echo "JSON ARM Output is required." 
+  exit 1
+fi
+
+for row in $(echo "${ARM_OUTPUT}" | jq -c '. | to_entries[]'); do
+  outputName=$(echo ${row} | jq -r ".key")
+  outputValue=$(echo ${row} | jq -r ".value.value")
+  echo "##vso[task.setvariable variable=${outputName};isOutput=true]${outputValue}"
+done
