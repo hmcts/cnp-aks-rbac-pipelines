563dbc852b87ee43a882afc7d578046626e6c479
Leonardo Fernandez <leonfs@gmail.com>
2019-02-23 22:52:05 +0000

    testing the Az Function

--- tests/rbac.tests.ps1
+++ tests/rbac.tests.ps1
@@ -13,7 +13,8 @@ Describe 'RBAC Model' {
     Context 'Test Run' {
         It 'Deployment Target Resource Group Exists' {
             Write-Host $ResourceGroupName
-            Get-AzureRMResourceGroup -Name $ResourceGroupName -ErrorAction SilentlyContinue  | Should Not BeNullOrEmpty
+            Get-AzureRMResourceGroup -Name $ResourceGroupName
+            # Get-AzureRMResourceGroup -Name $ResourceGroupName -ErrorAction SilentlyContinue  | Should Not BeNullOrEmpty
         }
     }
 
