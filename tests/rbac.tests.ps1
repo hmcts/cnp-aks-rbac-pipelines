4ce3c888ded340c1576cc935865efc8ba54bfaa3
Leonardo Fernandez <leonfs@gmail.com>
2019-02-25 21:14:31 +0000

    Both tests should pass. Using BeforeAll

--- tests/rbac.tests.ps1
+++ tests/rbac.tests.ps1
@@ -23,14 +23,21 @@ Describe 'RBAC Model' {
     }
 
     Context 'Developer' {
-        It 'It should not have access to services' {
+
+        BeforeAll {
             Set-Location ./tests/interactive-login-bypasser/
             $chrome_process = Start-Process -FilePath "./latest/chrome" -ArgumentList "--headless","--disable-gpu","--remote-debugging-port=9222" -PassThru
             npm install
             node index.js $AgentTempDirectory/test-developer.json
             $chrome_process.Kill()
+        }
 
-            { kubectl get services } | Should -Throw
+        It 'It should not have access to services' {
+            kubectl get services | Should BeNullOrEmpty
+        }
+
+        It 'It should have access to pods' {
+            kubectl get pods | Should Not BeNullOrEmpty
         }
     }
 }
