1f4ac040de13aaa089d04c903786f0019c29473a
Leonardo Fernandez <leonfs@gmail.com>
2019-02-25 16:23:11 +0000

    Adapted test to powershell

--- tests/rbac.tests.ps1
+++ tests/rbac.tests.ps1
@@ -25,9 +25,10 @@ Describe 'RBAC Model' {
     Context 'Develeloper' {
         It 'It should run in PWSH' {
             ./latest/chrome --headless --disable-gpu --remote-debugging-port=9222 https://www.chromestatus.com &
-            export CHROME_PID=$!
-            npm install && node index.js $(Agent.TempDirectory)/test-developer.json
-            kill -9 $CHROME_PID
+            $chrome_pid=$!
+            npm install
+            node index.js $AgentTempDirectory/test-developer.json
+            Stop-Process -9 $chrome_pid
 
             kubectl get namespaces
         }
