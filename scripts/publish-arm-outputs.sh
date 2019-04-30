81cdd5258c4a703e5ce1820283a3298773e99584
Martin Palmer <martin.palmer@almmechanics.com>
2019-04-30 09:22:48 +0100

    Modified the shape for the variable export script

--- scripts/publish-arm-outputs.sh
+++ scripts/publish-arm-outputs.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
-
-ARM_OUTPUT_VARIABLE=$1
-
-for row in $(echo '$(ARM_OUTPUT_VARIABLE)' | jq -c '. | to_entries[]'); do
+INPUT_VARIABLE="${1}"
+for row in $(echo "${INPUT_VARIABLE}" | jq -c '. | to_entries[]'); do
     outputName=$(echo ${row} | jq -r ".key")
     outputValue=$(echo ${row} | jq -r ".value.value")
     echo "##vso[task.setvariable variable=${outputName}]${outputValue}"
