b8c6d13eab9b93aac8f9763166c7f2de42fdc8b2
Martin Palmer <martin.palmer@almmechanics.com>
2019-04-30 21:47:37 +0100

    disabled validation

--- scripts/create-developer-roles.sh
+++ scripts/create-developer-roles.sh
@@ -4,7 +4,7 @@ set -e
 ROLES_YAML=${1:-templates/repositories.yaml}
 BINDING_YAML=${2:-roles/developers-log-reader-binding.yaml}
 
-kubectl apply -f ${ROLES_YAML}
+kubectl apply -f ${ROLES_YAML} --validate=false
 DEVELOPERS_GROUP=$(az ad group list --query  "[?displayName=='developers'].objectId" -o tsv)
 if [ ! -z "${DEVELOPERS_GROUP}" ]; then          
     cat ${BINDING_YAML} | sed -e 's@${DEVELOPERS_GROUP}@'"$DEVELOPERS_GROUP"'@' | kubectl apply -f -
