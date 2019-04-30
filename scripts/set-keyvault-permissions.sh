b658bcec5b413a31da23a8db8f30bf2bf8e2fade
Martin Palmer <martin.palmer@almmechanics.com>
2019-04-30 16:15:14 +0100

    Add keyvault permissions

---
+++ scripts/set-keyvault-permissions.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+KEYVAULT_NAME=$1
+MI_PRINCIPAL_ID=$2        
+        
+az keyvault set-policy -n ${KEYVAULT_NAME} --object-id ${MI_PRINCIPAL_ID} --secret-permissions get list --certificate-permissions get list --key-permissions get list
