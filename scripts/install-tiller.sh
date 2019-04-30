5c91fe76d8d4c8360c538d32c89acfdd96a2fcd2
Martin Palmer <martin.palmer@almmechanics.com>
2019-04-30 16:18:21 +0100

    add install tiller

---
+++ scripts/install-tiller.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+CLUSTER_NAME=$1
+HELM_VERSION=$2
+HISTORY_MAX=${3:5}
+RBAC_CONFIG=${4:-roles/helm-rbac-config.yaml}
+
+kubectl apply -f ${RBAC_CONFIG}
+helm init --wait --service-account tiller --tiller-image gcr.io/kubernetes-helm/tiller:v${HELM_VERSION} --history-max=${HISTORY_MAX} --kube-context "${CLUSTER_NAME}-admin"
