e49a350c1a0243ebb1645307883fc7b3669765f6
Martin Palmer <martin.palmer@almmechanics.com>
2019-04-30 22:16:03 +0100

    simplified error handling for tiller

--- scripts/install-tiller.sh
+++ scripts/install-tiller.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
-set -e
 
 CLUSTER_NAME=$1
 HELM_VERSION=$2
 HISTORY_MAX=${3:-5}
 RBAC_CONFIG=${4:-roles/helm-rbac-config.yaml}
 
-kubectl create -f ${RBAC_CONFIG}
+kubectl apply -f ${RBAC_CONFIG}
 helm init --wait --service-account tiller --tiller-image gcr.io/kubernetes-helm/tiller:v${HELM_VERSION} --history-max=${HISTORY_MAX} --kube-context ${CLUSTER_NAME}-admin
