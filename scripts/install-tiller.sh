f712d2c478df18bf7045f0956bec394e0db391d8
Martin Palmer <martin.palmer@almmechanics.com>
2019-04-30 20:34:15 +0100

    changed tiller config

--- scripts/install-tiller.sh
+++ scripts/install-tiller.sh
@@ -7,4 +7,4 @@ HISTORY_MAX=${3:-5}
 RBAC_CONFIG=${4:-roles/helm-rbac-config.yaml}
 
 kubectl apply -f ${RBAC_CONFIG}
-helm init --wait --service-account tiller --tiller-image gcr.io/kubernetes-helm/tiller:v${HELM_VERSION} --history-max=${HISTORY_MAX} --kube-context "${CLUSTER_NAME}-admin"
+helm init --wait --service-account tiller --tiller-image gcr.io/kubernetes-helm/tiller:v${HELM_VERSION} --history-max=${HISTORY_MAX} --kube-context ${CLUSTER_NAME}-admin
