560fcb98a0a833d7536acd73aac012c650a9fcef
Martin Palmer <martin.palmer@almmechanics.com>
2019-05-02 14:42:28 +0100

    remove path for secret file

--- scripts/get-githubkey.sh
+++ scripts/get-githubkey.sh
@@ -2,8 +2,7 @@
 set -e
 
 FLUX_GITHUB_FILE=${1}
-WORK_FOLDER=${2:-.}
 
 kubectl -n admin delete secret flux-git-deploy || true
-kubectl apply -f ${WORK_FOLDER}/${FLUX_GITHUB_FILE}
+kubectl apply -f ${FLUX_GITHUB_FILE}
 kubectl -n admin delete $(kubectl get pod -o name -l app=flux -n admin)
