fece4e84bfcb48aa2f091ca5f614f6c774f9c821
Martin Palmer <martin.palmer@almmechanics.com>
2019-05-02 12:23:56 +0100

    change to WORK_FOLDER

--- scripts/get-githubkey.sh
+++ scripts/get-githubkey.sh
@@ -2,8 +2,8 @@
 set -e
 
 FLUX_GITHUB_FILE=${1}
-TEMP_FOLDER=${2:-$(Agent.TempDirectory)}
+WORK_FOLDER=${2:-.)}
 
 kubectl -n admin delete secret flux-git-deploy || true
-kubectl apply -f ${TEMP_FOLDER}/${FLUX_GITHUB_FILE}
+kubectl apply -f ${WORK_FOLDER}/${FLUX_GITHUB_FILE}
 kubectl -n admin delete $(kubectl get pod -o name -l app=flux -n admin)
