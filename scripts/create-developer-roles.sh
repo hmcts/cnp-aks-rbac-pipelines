e0978c62ba2e7350c999d1ffc4937605d983482b
Martin Palmer <martin.palmer@almmechanics.com>
2019-05-09 15:04:26 +0100

    change binding for dev roles

--- scripts/create-developer-roles.sh
+++ scripts/create-developer-roles.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 set -e
 
-ROLES_YAML=${1:-templates/repositories.yaml}
-BINDING_YAML=${2:-roles/developers-log-reader-binding.yaml}
+BINDING_YAML=${1:-roles/developers-log-reader-role.yaml}
 
-kubectl apply -f ${ROLES_YAML} --validate=false
+kubectl apply -f ${BINDING_YAML} --validate=false
 DEVELOPERS_GROUP=$(az ad group list --query  "[?displayName=='developers'].objectId" -o tsv)
 if [ ! -z "${DEVELOPERS_GROUP}" ]; then          
     cat ${BINDING_YAML} | sed -e 's@${DEVELOPERS_GROUP}@'"$DEVELOPERS_GROUP"'@' | kubectl apply -f -
