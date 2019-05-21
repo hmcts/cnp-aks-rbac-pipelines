7d5220aa60910579d3428154c08dade86050b4a1
Martin Palmer <martin.palmer@almmechanics.com>
2019-05-21 20:04:30 +0100

    Use az context for detemining subscription Set defaults for the names and prefixes

--- scripts/create-developer-roles.sh
+++ scripts/create-developer-roles.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 set -e
 
-SUBSCRIPTION_NAME=$1
-
 BINDING_YAML=${1:-roles/developers-log-reader-role.yaml}
+AKS_USER_GROUP_PREFIX=${2:-dcd_group_aks_user_}
+
+SUBSCRIPTION_NAME=$(az account show --query name -o tsv)
 
 kubectl apply -f ${BINDING_YAML} --validate=false
-DEVELOPERS_GROUP=$(az ad group list --query  "[?displayName=='dcd_group_aks_user_${SUBSCRIPTION_NAME,,}_v2'].objectId" -o tsv)
+DEVELOPERS_GROUP=$(az ad group list --query "[?displayName=='${AKS_USER_GROUP_PREFIX}${SUBSCRIPTION_NAME,,}_v2'].objectId" -o tsv)
 if [ ! -z "${DEVELOPERS_GROUP}" ]; then
     cat ${BINDING_YAML} | sed -e 's@${DEVELOPERS_GROUP}@'"$DEVELOPERS_GROUP"'@' | kubectl apply -f -
 fi
