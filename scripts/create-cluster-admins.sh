92c576e34161612b504b4b379bfebd4a6a2692b6
Martyn Skene Ashworth <martyn.ashworth@hmcts.net>
2019-05-21 19:27:59 +0100

    fixing rbac names

--- scripts/create-cluster-admins.sh
+++ scripts/create-cluster-admins.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 set -e
 
-CLUSTER_NAME=$1
+SUBSCRIPTION_NAME=$1
 CLUSTER_ADMIN_BINDING=${2:-roles/cluster-admin-role-binding.yaml}
 
-CLUSTER_GLOBAL_ADMINS_GROUP=$(az ad group list --query  "[?displayName=='aks-cluster-admins'].objectId" -o tsv)
+CLUSTER_GLOBAL_ADMINS_GROUP=$(az ad group list --query  "[?displayName=='dcd_group_aks_admin_global_v2'].objectId" -o tsv)
 
-CLUSTER_ADMINS_GROUP_NAME="${CLUSTER_NAME}-cluster-admins"
+CLUSTER_ADMINS_GROUP_NAME="dcd_group_aks_admin_${SUBSCRIPTION_NAME,,}_v2"
 CLUSTER_ADMIN_GROUP=$(az ad group list --query  "[?displayName=='${CLUSTER_ADMINS_GROUP_NAME}'].objectId" -o tsv)
-if [ -z "${CLUSTER_ADMIN_GROUP}" ]; then 
-    echo "Cluster admin group doesn't exist, creating"
-    CLUSTER_ADMIN_GROUP=$(az ad group create  --display-name ${CLUSTER_ADMINS_GROUP_NAME} --mail-nickname ${CLUSTER_ADMINS_GROUP_NAME} --query objectId -o tsv)
+if [ -z "${CLUSTER_ADMIN_GROUP}" ]; then
+    echo "Cluster admin group doesn't exist, aborting"
+    exit 1
 fi
 
 cat ${CLUSTER_ADMIN_BINDING} | \
--- scripts/create-developer-roles.sh
+++ scripts/create-developer-roles.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 set -e
 
+SUBSCRIPTION_NAME=$1
+
 BINDING_YAML=${1:-roles/developers-log-reader-role.yaml}
 
 kubectl apply -f ${BINDING_YAML} --validate=false
-DEVELOPERS_GROUP=$(az ad group list --query  "[?displayName=='developers'].objectId" -o tsv)
-if [ ! -z "${DEVELOPERS_GROUP}" ]; then          
+DEVELOPERS_GROUP=$(az ad group list --query  "[?displayName=='dcd_group_aks_user_${SUBSCRIPTION_NAME,,}_v2'].objectId" -o tsv)
+if [ ! -z "${DEVELOPERS_GROUP}" ]; then
     cat ${BINDING_YAML} | sed -e 's@${DEVELOPERS_GROUP}@'"$DEVELOPERS_GROUP"'@' | kubectl apply -f -
 fi
