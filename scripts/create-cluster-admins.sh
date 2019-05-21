abebd15b7ef5b642f3bee23e422b14379a15a65f
Martin Palmer <martin.palmer@almmechanics.com>
2019-05-21 21:07:37 +0100

    added verbose output to create cluster admins

--- scripts/create-cluster-admins.sh
+++ scripts/create-cluster-admins.sh
@@ -5,10 +5,10 @@ CLUSTER_ADMIN_BINDING=${1:-roles/cluster-admin-role-binding.yaml}
 AAD_ADMIN_GROUP_NAME=${2:-dcd_group_aks_admin_global_v2}
 CLUSTER_ADMINS_GROUP_PREFIX=${3:-dcd_group_aks_admin_}
 
-SUBSCRIPTION_NAME=$(az account show --query name -o tsv)
-CLUSTER_GLOBAL_ADMINS_GROUP=$(az ad group list --query "[?displayName=='${AAD_ADMIN_GROUP_NAME}'].objectId" -o tsv)
+SUBSCRIPTION_NAME=$(az account show --query name --verbose -o tsv)
+CLUSTER_GLOBAL_ADMINS_GROUP=$(az ad group list --query "[?displayName=='${AAD_ADMIN_GROUP_NAME}'].objectId" --verbose -o tsv)
 CLUSTER_ADMINS_GROUP_NAME="${CLUSTER_ADMINS_GROUP_PREFIX}${SUBSCRIPTION_NAME,,}_v2"
-CLUSTER_ADMIN_GROUP=$(az ad group list --query  "[?displayName=='${CLUSTER_ADMINS_GROUP_NAME}'].objectId" -o tsv)
+CLUSTER_ADMIN_GROUP=$(az ad group list --query "[?displayName=='${CLUSTER_ADMINS_GROUP_NAME}'].objectId" --verbose -o tsv)
 
 if [ -z "${CLUSTER_ADMIN_GROUP}" ]; then
     echo "Cluster admin group doesn't exist, aborting"
