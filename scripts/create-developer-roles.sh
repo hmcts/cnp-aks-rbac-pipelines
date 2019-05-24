24d5066fe44806c12e2d2e608baafbdf5798e98b
Tim Jacomb <tim.jacomb@hmcts.net>
2019-05-24 16:57:52 +0100

    Fix developer role binding

--- scripts/create-developer-roles.sh
+++ scripts/create-developer-roles.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 set -e
 
-BINDING_YAML=${1:-roles/developers-log-reader-role.yaml}
+BINDING_YAML=${1:-roles/reader-role-binding.yaml}
 AKS_USER_GROUP_PREFIX=${2:-dcd_group_aks_user_}
 
 SUBSCRIPTION_NAME=$(az account show --query name -o tsv)
 
 kubectl apply -f ${BINDING_YAML} --validate=false
 DEVELOPERS_GROUP=$(az ad group list --query "[?displayName=='${AKS_USER_GROUP_PREFIX}${SUBSCRIPTION_NAME,,}_v2'].objectId" -o tsv)
-if [ ! -z "${DEVELOPERS_GROUP}" ]; then
-    cat ${BINDING_YAML} | sed -e 's@${DEVELOPERS_GROUP}@'"$DEVELOPERS_GROUP"'@' | kubectl apply -f -
-fi
+cat ${BINDING_YAML} | sed -e 's@${DEVELOPERS_GROUP}@'"${DEVELOPERS_GROUP}"'@' | kubectl apply -f -
