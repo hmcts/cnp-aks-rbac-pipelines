57670c4d0677e7edf17c6808d6f35a48ee075633
Tim Jacomb <tim.jacomb@hmcts.net>
2019-05-25 16:21:56 +0100

    Set author and use main repo

--- scripts/install-flux.sh
+++ scripts/install-flux.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 set -e
 
-CLUSTER_NAME=$1
+ENV=$1
 GIT_REPO=${2:-https://weaveworks.github.io/flux}
 VALUES=${3-deployments/weave-flux/values.yaml}
 
 helm repo add weaveworks ${GIT_REPO}
 helm upgrade flux weaveworks/flux --install --namespace admin -f ${VALUES} \
-    --set "git.path=k8s/${CLUSTER_NAME}\,k8s/common",git.label=${CLUSTER_NAME} \
+    --set "git.path=k8s/${ENV}\,k8s/common",git.label=${ENV},git.email=flux-${ENV}@hmcts.net,git.user="Flux ${ENV}" \
     --wait
