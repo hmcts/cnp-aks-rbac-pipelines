a48a7f011f753f50026bab0f2f40039a7533321c
Tim Jacomb <t.jacomb@kainos.com>
2019-05-28 08:07:57 +0100

    Update scripts/publish-mon-actiongroup.sh

--- scripts/publish-mon-actiongroup.sh
+++ scripts/publish-mon-actiongroup.sh
@@ -4,5 +4,5 @@ set -e
 AG_NAME=$1
 RESOURCE_GROUP_NAME=$2
 
-actionGroupID=$( az resource list --name ${AG_NAME} --resource-group ${RESOURCE_GROUP_NAME} --query "[?type=='Microsoft.Insights/actiongroups']" | jq '.[0].id')
+actionGroupID=$(az resource list --name ${AG_NAME} --resource-group ${RESOURCE_GROUP_NAME} --query "[?type=='Microsoft.Insights/actiongroups']" | jq '.[0].id')
 echo "##vso[task.setvariable variable=actionGroupID]${actionGroupID}"
