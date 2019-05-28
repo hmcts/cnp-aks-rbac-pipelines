544644fb5d903a7b157a24813703e66e50095238
Praveen Adusumilli <praveen.adusumilli@hmcts.net>
2019-05-28 14:20:15 +0100

    Added cluster specific folder for flux

--- scripts/install-flux.sh
+++ scripts/install-flux.sh
@@ -2,10 +2,11 @@
 set -e
 
 ENV=$1
-GIT_REPO=${2:-https://weaveworks.github.io/flux}
-VALUES=${3-deployments/weave-flux/values.yaml}
+CLUSTER_NAME=$2
+GIT_REPO=${3:-https://weaveworks.github.io/flux}
+VALUES=${4-deployments/weave-flux/values.yaml}
 
 helm repo add weaveworks ${GIT_REPO}
 helm upgrade flux weaveworks/flux --install --namespace admin -f ${VALUES} \
-    --set "git.path=k8s/${ENV}\,k8s/common",git.label=${ENV},git.email=flux-${ENV}@hmcts.net,git.user="Flux ${ENV}" \
+    --set "git.path=k8s/${ENV}\,k8s/${ENV}/${CLUSTER_NAME}\,k8s/common",git.label=${ENV},git.email=flux-${ENV}@hmcts.net,git.user="Flux ${ENV}" \
     --wait
