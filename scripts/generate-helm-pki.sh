4887b51d6bd8a9dd87e947634c6fcf6c65aacb39
Praveen Adusumilli <praveen.adusumilli@hmcts.net>
2019-06-03 14:49:55 +0100

    Adding scripts to optionally  enable tls on helm

---
+++ scripts/generate-helm-pki.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+set -e
+
+VAULT_NAME=$1
+
+openssl genrsa -out ./ca.key.pem 4096
+
+openssl req -key ca.key.pem -new -x509 -days 7300 -sha256 \
+-out ca.cert.pem -extensions v3_ca \
+-subj "/C=GB/ST=London/L=London/O=Ministry of Justice/OU=HMCTS/CN=helm.platform.hmcts.net" -nodes
+
+openssl genrsa -out ./tiller.key.pem 4096
+
+openssl genrsa -out ./helm.key.pem 4096
+
+openssl req -key tiller.key.pem -new -sha256 -out tiller.csr.pem \
+-subj "/C=GB/ST=London/L=London/O=Ministry of Justice/OU=HMCTS/CN=helm.platform.hmcts.net" -nodes
+
+openssl req -key helm.key.pem -new -sha256 -out helm.csr.pem \
+-subj "/C=GB/ST=London/L=London/O=Ministry of Justice/OU=HMCTS/CN=helm.platform.hmcts.net" -nodes
+
+echo subjectAltName=IP:127.0.0.1 > extfile.cnf
+
+openssl x509 -req -CA ca.cert.pem -CAkey ca.key.pem -CAcreateserial \
+-in tiller.csr.pem -out tiller.cert.pem -days 365 -extfile extfile.cnf
+
+openssl x509 -req -CA ca.cert.pem -CAkey ca.key.pem -CAcreateserial \
+-in helm.csr.pem -out helm.cert.pem -days 365
+
+# Generate secret container Helm certs for Flux while we're at it
+kubectl -n admin create secret tls helm-client-certs \
+--cert=helm.cert.pem \
+--key=helm.key.pem \
+--dry-run -o yaml > helm-client-certs.yaml
+
+function set_kv_secret {
+ az keyvault secret set \
+ --vault-name ${VAULT_NAME} \
+ --encoding ascii \
+ --name ${1} \
+ --file ${2} \
+ --description "${3}"
+}
+
+set_kv_secret helm-pki-ca-key      ca.key.pem             "Helm PKI CA private key"
+set_kv_secret helm-pki-ca-cert     ca.cert.pem            "Helm PKI CA certificate"
+set_kv_secret helm-pki-tiller-key  tiller.key.pem         "Helm PKI Tiller private key"
+set_kv_secret helm-pki-helm-key    helm.key.pem           "Helm PKI Helm private key"
+set_kv_secret helm-pki-tiller-csr  tiller.csr.pem         "Helm PKI Tiller CSR"
+set_kv_secret helm-pki-helm-csr    helm.csr.pem           "Helm PKI Helm CSR"
+set_kv_secret helm-pki-tiller-cert tiller.cert.pem        "Helm PKI Tiller certificate"
+set_kv_secret helm-pki-helm-cert   helm.cert.pem          "Helm PKI Helm certificate"
+set_kv_secret helm-pki-flux-secret helm-client-certs.yaml "Helm PKI Flux Secret"
+
+# Just to allow re-downloading later - may not need
+rm *.pem helm-client-certs.yaml
--- scripts/install-flux.sh
+++ scripts/install-flux.sh
@@ -5,8 +5,19 @@ ENV=$1
 CLUSTER_NAME=$2
 GIT_REPO=${3:-https://weaveworks.github.io/flux}
 VALUES=${4-deployments/weave-flux/values.yaml}
+VAULT_NAME=$5
+ENABLE_HELM_TLS=${6:false}
 
 helm repo add weaveworks ${GIT_REPO}
+
+if [[ ${ENABLE_HELM_TLS} == true ]]
+then
+ az keyvault secret download --vault-name ${VAULT_NAME} --encoding ascii --name helm-pki-flux-secret --file helm-client-certs.yaml
+ kubectl apply -f helm-client-certs.yaml
+ params+=(--tls --tls-verify --tls-ca-cert ca.cert.pem --tls-cert helm.cert.pem --tls-key helm.key.pem  --set helmOperator.tls.caContent="$(cat ca.cert.pem)" )
+fi
+
 helm upgrade flux weaveworks/flux --install --namespace admin -f ${VALUES} \
     --set "git.path=k8s/${ENV}/common\,k8s/${ENV}/${CLUSTER_NAME}\,k8s/common",git.label=${ENV},git.email=flux-${ENV}@hmcts.net,git.user="Flux ${ENV}" \
+    "${params[@]}"
     --wait
