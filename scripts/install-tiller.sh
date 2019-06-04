a385a1bed8cdcd779c1ed3dbe2120b7ae27c7297
Praveen Adusumilli <47391951+adusumillipraveen@users.noreply.github.com>
2019-06-04 21:33:50 +0100

    trying with different path similar to other files (#38)

--- scripts/install-tiller.sh
+++ scripts/install-tiller.sh
@@ -5,12 +5,13 @@ HELM_VERSION=$2
 HISTORY_MAX=${3:-5}
 ENABLE_HELM_TLS=$4
 RBAC_CONFIG=roles/helm-rbac-config.yaml
-
+#added for debugging
+ls -lash
 kubectl create -f ${RBAC_CONFIG}
 
 if [[ ${ENABLE_HELM_TLS} == true ]]
 then
- helm_tls_params+=(--tiller-tls --tiller-tls-cert tiller.cert.pem --tiller-tls-key tiller.key.pem --tiller-tls-verify --tls-ca-cert ca.cert.pem)
+ helm_tls_params+=(--tiller-tls --tiller-tls-cert scripts/tiller.cert.pem --tiller-tls-key scripts/tiller.key.pem --tiller-tls-verify --tls-ca-cert scripts/ca.cert.pem)
 fi
 
 helm init --wait --service-account tiller --tiller-image gcr.io/kubernetes-helm/tiller:v${HELM_VERSION} --history-max=${HISTORY_MAX} --kube-context ${CLUSTER_NAME}-admin "${helm_tls_params[@]}"
