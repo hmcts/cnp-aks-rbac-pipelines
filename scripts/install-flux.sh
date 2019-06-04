e84da38dbdf02874e81e929d9695c20a0c21eee7
Praveen Adusumilli <47391951+adusumillipraveen@users.noreply.github.com>
2019-06-04 08:54:49 +0100

    Apply suggestions from code review
    Co-Authored-By: Tim Jacomb <t.jacomb@kainos.com>

--- scripts/install-flux.sh
+++ scripts/install-flux.sh
@@ -14,10 +14,10 @@ if [[ ${ENABLE_HELM_TLS} == true ]]
 then
  az keyvault secret download --vault-name ${VAULT_NAME} --encoding ascii --name helm-pki-flux-secret --file helm-client-certs.yaml
  kubectl apply -f helm-client-certs.yaml
- params+=(--tls --tls-verify --tls-ca-cert ca.cert.pem --tls-cert helm.cert.pem --tls-key helm.key.pem  --set helmOperator.tls.caContent="$(cat ca.cert.pem)" )
+ helm_tls_params+=(--tls --tls-verify --tls-ca-cert ca.cert.pem --tls-cert helm.cert.pem --tls-key helm.key.pem  --set helmOperator.tls.caContent="$(cat ca.cert.pem)" )
 fi
 
 helm upgrade flux weaveworks/flux --install --namespace admin -f ${VALUES} \
     --set "git.path=k8s/${ENV}/common\,k8s/${ENV}/${CLUSTER_NAME}\,k8s/common",git.label=${ENV},git.email=flux-${ENV}@hmcts.net,git.user="Flux ${ENV}" \
-    "${params[@]}"
+    "${helm_tls_params[@]}"
     --wait
