60fe86cdbfa8466b2b033fc33138f692c6146bd8
Praveen Adusumilli <praveen.adusumilli@hmcts.net>
2019-06-05 09:03:25 +0100

    Add tls params when enabling tls

--- scripts/install-sealed-secrets.sh
+++ scripts/install-sealed-secrets.sh
@@ -2,8 +2,9 @@
 set -ex
 
 VAULT_NAME=$1
-VERSION=${2:-1.0.1}
-NAMESPACE=${3:-admin}
+VERSION=$2
+NAMESPACE=$3
+ENABLE_HELM_TLS=$4
 
 az keyvault secret download \
   --file sealed-secrets-pki.yaml \
@@ -13,5 +14,12 @@ az keyvault secret download \
 
 kubectl apply -f sealed-secrets-pki.yaml
 
-helm upgrade sealed-secrets stable/sealed-secrets --version ${VERSION} --install --recreate-pods --namespace ${NAMESPACE} --set secretName=sealed-secrets-pki --wait
+if [[ ${ENABLE_HELM_TLS} == true ]]
+then
+ helm_tls_params+=(--tls --tls-ca-cert scripts/ca.cert.pem --tls-cert scripts/helm.cert.pem --tls-key scripts/helm.key.pem )
+fi
+
+helm upgrade sealed-secrets stable/sealed-secrets --version ${VERSION} --install --recreate-pods --namespace ${NAMESPACE} \
+     --set secretName=sealed-secrets-pki --wait \
+     "${helm_tls_params[@]}"
 
