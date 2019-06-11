539f9379ddb1b092642cc6e593389e60f1b62111
Praveen Adusumilli <praveen.adusumilli@hmcts.net>
2019-06-11 12:58:12 +0100

    removing using of outputs from templates

--- scripts/publish-aks-clusterid.sh
+++ scripts/publish-aks-clusterid.sh
@@ -4,5 +4,5 @@ set -e
 CLUSTER_NAME=$1
 RESOURCE_GROUP_NAME=$2
 
-aksID=$(az resource list --name ${CLUSTER_NAME} --resource-group ${RESOURCE_GROUP_NAME} --query "[?type=='Microsoft.ContainerService/managedClusters']" | jq '.[0].id')
+aksID=$(az aks show -n ${CLUSTER_NAME} -g ${RESOURCE_GROUP_NAME} --query id -o tsv)
 echo "##vso[task.setvariable variable=aksid]${aksID}"
