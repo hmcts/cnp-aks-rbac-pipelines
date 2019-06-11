a61335821638cb6a0539b854d9374c15c45e818c
Praveen Adusumilli <praveen.adusumilli@hmcts.net>
2019-06-11 13:42:12 +0100

    removing outputs and referring from input

--- scripts/publish-aks-clusterid.sh
+++ scripts/publish-aks-clusterid.sh
@@ -4,5 +4,5 @@ set -e
 CLUSTER_NAME=$1
 RESOURCE_GROUP_NAME=$2
 
-aksID=$(az aks show -n ${CLUSTER_NAME} -g ${RESOURCE_GROUP_NAME} --query id -o tsv)
+aksID=$(az aks show --name ${CLUSTER_NAME} --resource-group ${RESOURCE_GROUP_NAME} --query id -o tsv)
 echo "##vso[task.setvariable variable=aksid]${aksID}"
