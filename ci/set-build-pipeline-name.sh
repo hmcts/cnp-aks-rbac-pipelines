1585e85bbdc0606e35c7d533799c9c08d9b6f318
Tim Jacomb <tim.jacomb@hmcts.net>
2019-06-12 11:32:42 +0100

    Longer date

--- ci/set-build-pipeline-name.sh
+++ ci/set-build-pipeline-name.sh
@@ -1,6 +1,4 @@
 #!/usr/bin/env bash
-REV=${BUILD_BUILDID}
-
 VSTS_COMMAND="vso[build.updatebuildnumber]"
-DATE=$(date "+%Y-%m-%d")
-echo "##${VSTS_COMMAND} PR-${SYSTEM_PULLREQUEST_PULLREQUESTNUMBER} ${DATE}${REV}"
+DATE=$(date -u +"%Y-%m-%dT%H:%M:%SZ")
+echo "##${VSTS_COMMAND} PR-${SYSTEM_PULLREQUEST_PULLREQUESTNUMBER} ${DATE}"
