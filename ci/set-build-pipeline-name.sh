836cad96fd0f4c9a1a99009fc39b7d00fd19b3a0
Tim Jacomb <tim.jacomb@hmcts.net>
2019-06-12 11:36:19 +0100

    Handle other branches

--- ci/set-build-pipeline-name.sh
+++ ci/set-build-pipeline-name.sh
@@ -1,4 +1,11 @@
 #!/usr/bin/env bash
 VSTS_COMMAND="vso[build.updatebuildnumber]"
 DATE=$(date -u +"%Y-%m-%dT%H:%M:%SZ")
-echo "##${VSTS_COMMAND} PR-${SYSTEM_PULLREQUEST_PULLREQUESTNUMBER} ${DATE}"
+
+BRANCH_NAME=${BUILD_SOURCEBRANCHNAME}
+
+if [ "${BUILD_REASON}" = "PullRequest" ] ; then
+  BRANCH_NAME=PR-${SYSTEM_PULLREQUEST_PULLREQUESTNUMBER}
+fi
+
+echo "##${VSTS_COMMAND} ${BRANCH_NAME} ${DATE}"
