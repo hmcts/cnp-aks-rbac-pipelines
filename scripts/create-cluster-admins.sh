ceec791453ee3e06c4c4b0bd4dad7114d60dba4f
Tim Jacomb <tim.jacomb@hmcts.net>
2019-06-13 09:08:03 +0100

    Other test

--- scripts/create-cluster-admins.sh
+++ scripts/create-cluster-admins.sh
@@ -4,7 +4,7 @@ set -ex
 CLUSTER_ADMIN_BINDING=${1:-roles/cluster-admin-role-binding.yaml}
 AAD_ADMIN_GROUP_NAME=${2:-dcd_group_aks_admin_global_v2}
 CLUSTER_ADMINS_GROUP_PREFIX=${3:-dcd_group_aks_admin_}
-
+   
 SUBSCRIPTION_NAME=$(az account show --query name --verbose -o tsv)
 CLUSTER_GLOBAL_ADMINS_GROUP=$(az ad group list --query "[?displayName=='${AAD_ADMIN_GROUP_NAME}'].objectId" --verbose -o tsv)
 CLUSTER_ADMINS_GROUP_NAME="${CLUSTER_ADMINS_GROUP_PREFIX}${SUBSCRIPTION_NAME,,}_v2"
