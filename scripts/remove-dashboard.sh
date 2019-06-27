d03bfb78191edc039baecf7d77bf4ca0b2dd4af9
Praveen Adusumilli <praveen.adusumilli@hmcts.net>
2019-06-27 08:57:51 +0100

    Fixing disabling dashboard

--- scripts/remove-dashboard.sh
+++ scripts/remove-dashboard.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 set -e
 
-# disabled due to: https://github.com/Azure/AKS/issues/786
-#kubectl --namespace kube-system delete deployment kubernetes-dashboard
-#kubectl --namespace  kube-system delete svc kubernetes-dashboard
+CLUSTER_NAME=$1
+RESOURCE_GROUP_NAME=$2
+
+az aks disable-addons -a kube-dashboard --name ${CLUSTER_NAME} --resource-group ${RESOURCE_GROUP_NAME} --no-wait
