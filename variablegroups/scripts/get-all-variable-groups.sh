8cb83017aa3f633142bcba7ca7fbddbcbe87a92e
Praveen Adusumilli <praveen.adusumilli@hmcts.net>
2019-07-03 20:01:26 +0100

    Adding all env variable groups

--- variablegroups/scripts/get-all-variable-groups.sh
+++ variablegroups/scripts/get-all-variable-groups.sh
@@ -10,11 +10,11 @@ fi
 ACCESSTOKEN=$1
 USERNAME=${2:azuredevops}
 
-groupList=$(curl -X GET -s -u ${USERNAME}:${ACCESSTOKEN} "https://dev.azure.com/hmcts/cnp/_apis/distributedtask/variablegroups/?api-version=5.0-preview.1" | jq -r '.value[] | .name' )
+groupList=$(curl -X GET -s -u ${USERNAME}:${ACCESSTOKEN} "https://dev.azure.com/hmcts/cnp/_apis/distributedtask/variablegroups/?api-version=5.0-preview.1" | jq -r '.value[] | .name  | if contains("AKS") then . else empty end' )
 
 
 for group in ${groupList} ; do
 
-curl -X GET -s -u ${USERNAME}:${ACCESSTOKEN} "https://dev.azure.com/hmcts/cnp/_apis/distributedtask/variablegroups/?groupName=${group}&api-version=5.0-preview.1" | jq -r '.value[0] | del(.createdOn, .modifiedBy , .createdBy, .modifiedOn, .providerData.lastRefreshedOn)' > ${group}.json
+curl -X GET -s -u ${USERNAME}:${ACCESSTOKEN} "https://dev.azure.com/hmcts/cnp/_apis/distributedtask/variablegroups/?groupName=${group}&api-version=5.0-preview.1" | jq -r '.value[0] | del(.createdOn, .modifiedBy , .createdBy, .modifiedOn)' > ${group}.json
 echo "Updated ${group}"
 done
