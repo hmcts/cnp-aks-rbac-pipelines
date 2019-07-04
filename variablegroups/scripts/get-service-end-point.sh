12a4665d2420c3b501f89b4cf41acbb4a6764bb3
Praveen Adusumilli <47391951+adusumillipraveen@users.noreply.github.com>
2019-07-04 11:51:00 +0100

    Adding all env variable groups (#78)

---
+++ variablegroups/scripts/get-service-end-point.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+set -e
+
+if [ $# -lt 1 ]
+  then
+    echo "Not enough arguments supplied, pass UserName and Azure Personal Access token"
+    exit 1
+fi
+ACCESSTOKEN=$1
+ENDPOINTNAME=$2
+USERNAME=${3:azuredevops}
+
+echo $(curl -X GET -s -u ${USERNAME}:${ACCESSTOKEN}  "https://dev.azure.com/hmcts/cnp/_apis/serviceendpoint/endpoints?api-version=5.0-preview.2") | jq -r --arg endpointName ${ENDPOINTNAME} '.value[] | select(.name ==  $endpointName ) | .id'
