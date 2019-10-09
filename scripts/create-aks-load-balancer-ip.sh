27772e592418df45c563f157150e443ded0b3d3c
Praveen Adusumilli <praveen.adusumilli@hmcts.net>
2019-10-09 07:43:06 +0100

    Moving public ip creation to az cli as arm template is failing when not created upfront.

---
+++ scripts/create-aks-load-balancer-ip.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+set -e
+
+RESOURCE_GROUP_NAME=$1
+CLUSTER_NAME=$2
+#creating it ahead of aks arm template as arm template doesn't validate very well.
+az network public-ip create --name ${CLUSTER_NAME}-pip --resource-group ${RESOURCE_GROUP_NAME}
