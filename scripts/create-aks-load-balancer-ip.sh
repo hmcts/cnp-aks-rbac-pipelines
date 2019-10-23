1efe073d6a5fd9cbf827c1d7d5aa5b86e7549250
Praveen Adusumilli <praveen.adusumilli@hmcts.net>
2019-10-23 08:57:53 +0100

    Adding sku explicitly

--- scripts/create-aks-load-balancer-ip.sh
+++ scripts/create-aks-load-balancer-ip.sh
@@ -4,4 +4,4 @@ set -e
 RESOURCE_GROUP_NAME=$1
 CLUSTER_NAME=$2
 #creating it ahead of aks arm template as arm template doesn't validate very well.
-az network public-ip create --name ${CLUSTER_NAME}-pip --resource-group ${RESOURCE_GROUP_NAME}
+az network public-ip create --name ${CLUSTER_NAME}-pip --resource-group ${RESOURCE_GROUP_NAME} --sku Standard
