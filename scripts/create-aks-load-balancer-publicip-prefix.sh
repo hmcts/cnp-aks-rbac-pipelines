56b8013997f548ebbeba41f0bdd6ba396419c8fd
Praveen Adusumilli <praveen.adusumilli@hmcts.net>
2019-11-05 09:38:32 +0000

    commenting deleting old ip for now

--- scripts/create-aks-load-balancer-publicip-prefix.sh
+++ scripts/create-aks-load-balancer-publicip-prefix.sh
@@ -6,7 +6,7 @@ CLUSTER_NAME=$2
 #creating it ahead of aks arm template as arm template doesn't validate very well.
 
 #deleting with arm template.
-az network public-ip delete --name ${CLUSTER_NAME}-pip --resource-group ${RESOURCE_GROUP_NAME}
+#az network public-ip delete --name ${CLUSTER_NAME}-pip --resource-group ${RESOURCE_GROUP_NAME}
 
 #creating it ahead of aks arm template as arm template doesn't validate very well.
 az network public-ip prefix create --length 8 --name ${CLUSTER_NAME}-pip --resource-group ${RESOURCE_GROUP_NAME}
