75a5d023d3a9ef1026ee242e9dfd3236e10d5224
Praveen Adusumilli <47391951+adusumillipraveen@users.noreply.github.com>
2020-01-21 13:42:43 +0000

    Cleaning up old helm config (#256)

--- scripts/install-flux.sh
+++ scripts/install-flux.sh
@@ -11,7 +11,7 @@ FLUX_HELM_CRD=https://raw.githubusercontent.com/fluxcd/helm-operator/chart-0.5.0
 helm repo add fluxcd ${HELM_REPO}
 
 
-helm upgrade flux fluxcd/flux --install --recreate-pods --namespace admin -f ${VALUES} --version 1.1.0 --set image.tag=1.17.1 \
+helm upgrade flux fluxcd/flux --install --namespace admin -f ${VALUES} --version 1.1.0 --set image.tag=1.17.1 \
     --set "git.path=k8s/${ENV}/common\,k8s/${ENV}/${CLUSTER_NAME}\,k8s/${ENV}/${CLUSTER_NAME}-overlay\,k8s/common",git.label=${ENV},git.email=flux-${ENV}@hmcts.net,git.user="Flux ${ENV}" \
     --wait
 
