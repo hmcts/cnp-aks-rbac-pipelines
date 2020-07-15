7b658623b5b511214d667c05bd52c45debf9271c
Praveen Adusumilli <praveen.adusumilli@hmcts.net>
2020-07-15 20:33:17 +0100

    add common overlay folder

--- scripts/install-flux.sh
+++ scripts/install-flux.sh
@@ -12,7 +12,7 @@ helm repo add fluxcd ${HELM_REPO}
 
 
 helm upgrade flux fluxcd/flux --install --namespace admin -f ${VALUES} --version 1.3.0 --set image.tag=1.19.0 \
-    --set "git.path=k8s/${ENV}/common\,k8s/${ENV}/${CLUSTER_NAME}\,k8s/${ENV}/${CLUSTER_NAME}-overlay\,k8s/common",git.label=${ENV},git.email=flux-${ENV}@hmcts.net,git.user="Flux ${ENV}" \
+    --set "git.path=k8s/${ENV}/common\,k8s/${ENV}/${CLUSTER_NAME}\,k8s/${ENV}/${CLUSTER_NAME}-overlay\,k8s/common\k8s/${ENV}/common-overlay\",git.label=${ENV},git.email=flux-${ENV}@hmcts.net,git.user="Flux ${ENV}" \
     --wait
 
 kubectl apply -f ${FLUX_HELM_CRD}
