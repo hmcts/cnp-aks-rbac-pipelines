d630955411d08e2aef57766f156e7f09cae5e0ea
Praveen Adusumilli <47391951+adusumillipraveen@users.noreply.github.com>
2020-07-29 15:20:48 +0100

    Upgrade flux (#393)

--- scripts/install-flux.sh
+++ scripts/install-flux.sh
@@ -11,7 +11,7 @@ FLUX_HELM_CRD=https://raw.githubusercontent.com/fluxcd/helm-operator/chart-1.1.0
 helm repo add fluxcd ${HELM_REPO}
 
 
-helm upgrade flux fluxcd/flux --install --namespace admin -f ${VALUES} --version 1.3.0 --set image.tag=1.19.0 \
+helm upgrade flux fluxcd/flux --install --namespace admin -f ${VALUES} --version 1.4.0 --set image.tag=1.20.0 \
     --set "git.path=k8s/${ENV}/common\,k8s/${ENV}/${CLUSTER_NAME}\,k8s/${ENV}/${CLUSTER_NAME}-overlay\,k8s/${ENV}/common-overlay\,k8s/common",git.label=${ENV},git.email=flux-${ENV}@hmcts.net,git.user="Flux ${ENV}" \
     --wait
 
