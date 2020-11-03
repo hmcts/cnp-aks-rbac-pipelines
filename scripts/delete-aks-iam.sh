3e49410c708c35f06d555e23b479f4171032915c
Thomas Thornton <>
2020-11-03 10:00:38 +0000

    Update bash script to reference id

--- scripts/delete-aks-iam.sh
+++ scripts/delete-aks-iam.sh
@@ -10,23 +10,25 @@ guids=(${GUIDS_LIST})
 IFS=","
 subs=(${SUBSCRIPTIONS_LIST})
 
-
 for sub in ${subs[@]}; 
 do
+
 echo "Setting subscription for $sub"
 az account set --subscription $sub
 
 for guid in ${guids[@]}; 
 do 
 
-ID=$(az role assignment list --all --query "[?name=='$guid'].[principalId]" -o tsv);
-SCOPE=$(az role assignment list --all --query "[?name=='$guid'].[scope]" -o tsv);
-RESOURCEGROUP=$(az role assignment list --all --query "[?name=='$guid'].[resourceGroup]" -o tsv);
+    ID=$(az role assignment list --all --query "[?name=='$guid'].[id]" -o tsv);
+    RESOURCEGROUP=$(az role assignment list --all --query "[?name=='$guid'].[resourceGroup]" -o tsv);
+
+    echo "Checking IAM Role Assignment for GUID:- $guid on subscription:- $sub in Resource Group:- $RESOURCEGROUP";
 
 if [ -n "${ID}" ];  then
 
     echo "Deleting IAM Role Assignment for GUID:- $guid on subscription:- $sub in Resource Group:- $RESOURCEGROUP";
-    az role assignment delete --assignee $ID --scope $SCOPE;
+    az role assignment delete --ids $ID;
+    
 fi
 
 done
