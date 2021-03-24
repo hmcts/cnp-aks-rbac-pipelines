3716ff7613f140820ac4d2a729ba0d8f766a2579
Thomas Thornton <>
2021-03-24 16:16:12 +0000

    upgrade az version

--- scripts/create-aks-load-balancer-publicip-prefix.sh
+++ scripts/create-aks-load-balancer-publicip-prefix.sh
@@ -9,4 +9,5 @@ CLUSTER_NAME=$2
 #az network public-ip delete --name ${CLUSTER_NAME}-pip --resource-group ${RESOURCE_GROUP_NAME}
 
 #creating it ahead of aks arm template as arm template doesn't validate very well.
+az upgrade --yes
 az network public-ip prefix create --length 28 --name ${CLUSTER_NAME}-pip --resource-group ${RESOURCE_GROUP_NAME} --zone 1 2 3
