a7715cbd1f64891bd4e4598355ae48085d075ff4
Praveen Adusumilli <praveen.adusumilli@hmcts.net>
2021-04-29 16:35:00 +0100

    Deleting resource lock when deleting cluster

--- scripts/delete-aks-cluster.sh
+++ scripts/delete-aks-cluster.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
-set -e
+set -ex
 
 RESOURCE_GROUP_NAME=$1
 CLUSTER_NAME=$2
+
+NODE_RG=$(az aks show -n ${CLUSTER_NAME} -g ${RESOURCE_GROUP_NAME} --query 'nodeResourceGroup' -o tsv)
+
+LOCKS=$(az group lock list -g $NODE_RG --query '[].name' -o json -o tsv)
+
+for RG_LOCK in ${LOCKS[@]};
+do
+  az lock delete --name $RG_LOCK --resource-group $NODE_RG
+done
 
 az aks delete -g ${RESOURCE_GROUP_NAME} -n ${CLUSTER_NAME} -y
