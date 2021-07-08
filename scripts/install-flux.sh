519cccb75ce4c1bcd8025f4fd84e8097afb42ebb
Praveen Adusumilli <praveen.adusumilli@hmcts.net>
2021-07-08 17:01:17 +0100

    Flux v2

--- scripts/install-flux.sh
+++ scripts/install-flux.sh
@@ -1,19 +1,27 @@
 #!/bin/bash
 set -ex
 
-ENV=$1
-CLUSTER_NAME=$2
-VAULT_NAME=$3
-HELM_REPO=https://charts.fluxcd.io
-VALUES=deployments/fluxcd/values.yaml
-FLUX_HELM_CRD=https://raw.githubusercontent.com/fluxcd/helm-operator/chart-1.2.0/deploy/crds.yaml
 
-helm repo add fluxcd ${HELM_REPO}
+CLUSTER_ENV=$1
+CLUSTER_FULLNAME=$2
+FLUX_CONFIG_URL=https://raw.githubusercontent.com/hmcts/cnp-flux-config/master
 
-kubectl apply -f ${FLUX_HELM_CRD}
-kubectl -n admin delete secret flux-helm-repositories || true
-helm upgrade flux-helm-operator fluxcd/helm-operator --install --namespace admin   -f  deployments/fluxcd/helm-operator-values.yaml --version 1.2.0 --wait
+if [ ${CLUSTER_ENV}=="mgmt-sandbox" ]; then
+  ENV="sbox-intsvc"
+else 
+  ENV=${CLUSTER_ENV}
+fi
 
+CLUSTER_NAME=echo ${CLUSTER_FULLNAME} | cut -d'-' -f 2
+
+# Install Flux
+kubectl apply -f ${FLUX_CONFIG_URL}/apps/flux-system/base/gotk-components.yaml
+
+#Git credentials
+kubectl apply -f ${FLUX_CONFIG_URL}/apps/flux-system/${ENV}/base/git-credentials.yaml
+
+#Create Flux Sync CRDs
+kubectl apply -f ${FLUX_CONFIG_URL}/apps/flux-system/base/flux-config-gitrepo.yaml
 
 #Install kustomize
 curl -s "https://raw.githubusercontent.com/\
@@ -25,16 +33,15 @@ mkdir -p $TMP_DIR
 cat <<EOF
 apiVersion: kustomize.config.k8s.io/v1beta1
 kind: Kustomization
-namespace: admin
+namespace: flux-system
 resources:
-  - https://raw.githubusercontent.com/hmcts/cnp-flux-config/master/k8s/namespaces/admin/flux/flux.yaml
+  - ${FLUX_CONFIG_URL}/apps/flux-system/base/kustomize.yaml
 patchesStrategicMerge:
-  - https://raw.githubusercontent.com/hmcts/cnp-flux-config/master/k8s/namespaces/admin/flux/patches/${ENV}/flux.yaml
-  - https://raw.githubusercontent.com/hmcts/cnp-flux-config/master/k8s/namespaces/admin/flux/patches/${ENV}/${CLUSTER_NAME}/flux.yaml
+  - ${FLUX_CONFIG_URL}/apps/flux-system/${ENV}/${CLUSTER_NAME}/kustomize.yaml
 EOF
 ) > "${TMP_DIR}/kustomization.yaml"
 # -----------------------------------------------------------
 
-kubectl apply -f https://raw.githubusercontent.com/hmcts/cnp-flux-config/master/k8s/$ENV/common/sealed-secrets/acr-credentials.yaml
-
 ./kustomize build ${TMP_DIR} |  kubectl apply -f -
+
+rm -rf kustomize
